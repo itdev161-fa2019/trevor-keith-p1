bcc85d3f0245526943f1c1a25a972cc54ca28a50
Trevor Keith <keithtw@gmatc.matc.edu>
2019-10-27 18:01:21 -0500

    Added database connection

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 // Initialize express application
 const app = express();
 
+//Connect database
+connectDatabase();
+
 // API endpoionts
 app.get('/', (req, res) =>
     res.send('HTTP get request successfully sent to root API endpoint')
+    
 );
 
 // Connection listener
